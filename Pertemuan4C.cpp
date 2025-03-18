b24c55d23c557cacd15c396d182e0d0afebeccf1
FarabyMalau <farabymuzakki49@gmail.com>
2025-03-18 17:09:37 +0600

    membuat menu pilihan

--- Pertemuan4C.cpp
+++ Pertemuan4C.cpp
@@ -12,5 +12,42 @@ float LuasLingkaran(float r){
 int main(){
     int pilihan;
     float panjang, lebar, jejari;
+    
+    do{
+        cout << "======Menu======" << endl;
+        cout << "1. Menghitung Luas Persegi Panjang" << endl;
+        cout << "2. Menghitung Luas Lingkaran " << endl;
+        cout << "3. Exit" << endl;
+        cout << "Pilihan (1/2/3) : ";
+        cin >> pilihan;
+
+        switch(pilihan) {
+            case 1 :
+                cout << "Menghitung Luas Persegi Panjang" << endl;
+                cout << "Masukkan Panjang : ";
+                cin >> panjang;
+                cout << "Masukkan Lebar : ";
+                cin >> lebar;
+                cout << "Luas Persegi Panjang : " <<
+                LuasPersegiPanjang(panjang, lebar) << endl;
+                cout << endl;
+                break;
+
+            case 2 :
+                cout << "Menghitung Luas Lingkaran" << endl;
+                cout << "Masukkan Jejari : ";
+                cin >> jejari;
+                cout << "Luas Lingkaran : " <<
+                LuasLingkaran(jejari) << endl;
+                cout << endl;
+                break;
+            case 3 : 
+                break;
+            default :
+                cout << "Pilihan anda salah!" << endl;
+                cout << endl;
+                break;
+        }
+    }while(pilihan != 3);
 
 }
